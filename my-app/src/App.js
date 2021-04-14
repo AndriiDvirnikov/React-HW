05c7f3c6b49fe032fe1ae8cfc76192db161344a2
unknown <rostvorgame@gmail.com>
2021-04-14 15:14:26 +0300

    HW#17 React

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import Student from "./components/Student";
+import User from "./components/User"
+import './App.css';
+
+function App() {
+  console.log(User);
+  return (
+    <div className="App">
+      <Student name={User.name} nickName={User.nickName} age = {User.age} />
+    </div>
+  );
+}
+
+
+
+export default App;
